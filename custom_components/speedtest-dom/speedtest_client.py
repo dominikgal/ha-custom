0719a5db6ea35a8abb2c771624f0a943a7ec91c0
Dominik <dominik.gal@gmail.com>
2025-11-17 20:49:50 +0100

    Update speedtest_client.py
    some debugging

--- custom_components/speedtest-dom/speedtest_client.py
+++ custom_components/speedtest-dom/speedtest_client.py
@@ -1,19 +1,27 @@
 from __future__ import annotations
 from typing import Any
 from homeassistant.core import HomeAssistant
+import logging
+
+_LOGGER = logging.getLogger(__name__)
 
 class SpeedTestClient:
     def __init__(self, hass: HomeAssistant) -> None:
         self._hass = hass
 
     def _run_test_sync(self) -> dict[str, Any]:
-        import speedtest  # provided by requirement in manifest
-        st = speedtest.Speedtest()
-        st.get_servers()
-        st.get_best_server()
-        st.download()
-        st.upload(pre_allocate=False)
-        return st.results.dict()
+        try:
+            import speedtest  # provided by requirement in manifest
+            
+            st = speedtest.Speedtest()
+            st.get_servers()
+            st.get_best_server()
+            st.download()
+            st.upload(pre_allocate=False)
+            return st.results.dict()
+        except Exception as e:
+            _LOGGER.error("SpeedTest sync test failed: %s", e, exc_info=True)
+            raise
 
     async def async_run_test(self) -> dict[str, Any]:
         return await self._hass.async_add_executor_job(self._run_test_sync)
